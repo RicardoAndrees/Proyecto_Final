98c1605b79deb08a749d1b1c53669111b53abea8
Ricardo Garcés <rgarces2019@alu.uct.cl>
2023-11-16 02:33:44 -0300

    Codigo con el mapa y sprite
    se prueba sprite movimiento, prontamente se trabajara para moverse en las celdas del mapa

--- ventana.py
+++ ventana.py
@@ -14,12 +14,20 @@ class InterfazGrafica:
         pygame.init()
 
         self.ecosistema = ecosistema
-        self.screen_size = (800, 600)
+        self.screen_size = (736, 736)
         self.screen = pygame.display.set_mode(self.screen_size)
         pygame.display.set_caption("Simulador de Ecosistemas")
 
+        self.fondo = pygame.image.load("mapita.jpg") 
+        self.fondo = pygame.transform.scale(self.fondo, self.screen_size)
+
+        self.sprite = Sprite((0, 0), "tigreso.jpg")
+
     def dibujar_ecosistema(self):
-        pass
+        self.screen.blit(self.fondo, (0, 0))
+    
+    def dibujar_sprite(self, sprite):
+        self.screen.blit(sprite.image, sprite.rect)
 
     def ejecutar_interfaz(self):
         while True:
@@ -27,8 +35,19 @@ class InterfazGrafica:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                self.sprite.move('UP')
+            elif keys[pygame.K_DOWN]:
+                self.sprite.move('DOWN')
+            elif keys[pygame.K_LEFT]:
+                self.sprite.move('LEFT')
+            elif keys[pygame.K_RIGHT]:
+                self.sprite.move('RIGHT')
 
             self.dibujar_ecosistema()
+            self.dibujar_sprite(self.sprite)
             pygame.display.flip()
 
 #eventos            
@@ -56,7 +75,28 @@ class Ecosistema:
         #ejecutar un ciclo del simulador
         pass
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, initial_position, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = initial_position
+        self.speed = 1 
+        self.image.set_colorkey((255, 255, 255))
+
+    def move(self, direction):
+        if direction == 'UP':
+            self.rect.y -= self.speed
+        elif direction == 'DOWN':
+            self.rect.y += self.speed
+        elif direction == 'LEFT':
+            self.rect.x -= self.speed
+        elif direction == 'RIGHT':
+            self.rect.x += self.speed
+
+
 if __name__ == "__main__":
+    pygame.init()
     ecosistema = Ecosistema(rows=10, cols=10)
     ecosistema.populate_ecosystem()
 
