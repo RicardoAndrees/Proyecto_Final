08b03b83880379e8803fa4fcf5dd59bffd7ea229
Ricardo Garcés <rgarces2019@alu.uct.cl>
2023-11-27 03:51:53 -0300

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+import time
 
 class InterfazGrafica:
     def __init__(self):
@@ -14,21 +16,35 @@ class InterfazGrafica:
 
         self.sprite_m9 = pygame.image.load("m9.png")
 
-        self.num_celdas_x = 10
-        self.num_celdas_y = 10
+        self.num_celdas_x = 15
+        self.num_celdas_y = 15
         self.ancho_celda = self.screen_size[0] // self.num_celdas_x
         self.alto_celda = self.screen_size[1] // self.num_celdas_y
 
-
         self.valores_celdas = [[0 for _ in range(self.num_celdas_x)] for _ in range(self.num_celdas_y)]
-        self.posicion_sprite = [0, 0]
+        self.posicion_sprite = [random.randint(0, self.num_celdas_x - 1), random.randint(0, self.num_celdas_y - 1)]
+        self.direccion_sprite = None
+        self.tiempo_aleatorio = time.time()
+
+
+    def generar_direccion_aleatoria(self):
+        return random.choice([pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN])
+
+    def mover_sprite_aleatoriamente(self):
+        tiempo_actual = time.time()
+
+        if tiempo_actual - self.tiempo_aleatorio > 1:
+            self.direccion_sprite = self.generar_direccion_aleatoria()
+            self.tiempo_aleatorio = tiempo_actual
+
+        self.actualizar_posicion_sprite(self.direccion_sprite)
 
     def dibujar_ecosistema(self):
         self.screen.blit(self.fondo, (0, 0))
 
         for i in range(self.num_celdas_x):
             for j in range(self.num_celdas_y):
-                x = i * self.ancho_celda
+                x = i * self.ancho_celda 
                 y = j * self.alto_celda
 
                 pygame.draw.rect(self.screen, (255, 255, 255), (x, y, self.ancho_celda, self.alto_celda), 2)
@@ -50,6 +66,7 @@ class InterfazGrafica:
             nueva_posicion[1] += 1
 
         self.posicion_sprite = nueva_posicion
+        pygame.time.delay(2000)
 
     def ejecutar_interfaz(self):
         while True:
@@ -58,10 +75,7 @@ class InterfazGrafica:
                     pygame.quit()
                     sys.exit()
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-                        self.actualizar_posicion_sprite(event.key)
-
+            self.mover_sprite_aleatoriamente()
             self.dibujar_ecosistema()
             pygame.display.flip()
 
